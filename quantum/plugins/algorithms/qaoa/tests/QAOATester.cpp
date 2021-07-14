02cf3e931af60805cceb0a6c1f03fa02836e1211
milos_pr <milos_pr@nextgenio-login1.localdomain>
2021-07-14 13:43:50 +0100

    modules load

--- quantum/plugins/algorithms/qaoa/tests/QAOATester.cpp
+++ quantum/plugins/algorithms/qaoa/tests/QAOATester.cpp
@@ -456,7 +456,7 @@ TEST(QAOATester, checkMaxCutGrouping) {
 }*/
 
 TEST(QAOATester, checkP1TriangleGraphGroupingExpVal) {
-  auto acc = xacc::getAccelerator("quest", {{"shots", 8192}});
+  auto acc = xacc::getAccelerator("quest", {{"shots", 8192}, {"nbQubits", 3}});
   auto optimizer = xacc::getOptimizer(
       "nlopt",
       {{"maximize", true}, {"initial-parameters", random_vector(-2., 2., 2)}});
